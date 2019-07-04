81a5703d5ae7ace47c6c3875f44af5d4b3eeffb6
Roopesh R <44260545+LUKARS3@users.noreply.github.com>
2019-07-04 11:55:54 +0530

    Update prog-3.py

--- prog-3.py
+++ prog-3.py
@@ -1,6 +1,6 @@
-tup12 = ('u', 'a', 'o', 'i', 'e')
-ip12 = input().lower()
-if ip12 in tup12:
+tup123 = ('u', 'a', 'o', 'i', 'e')
+ip123 = input().lower()
+if ip123 in tup123:
   print('Vowel')
 else:
   print('Consonant')
