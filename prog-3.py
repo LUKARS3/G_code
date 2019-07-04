3b7a18f7e27a2b5b462c9373b6bd8f2f554a41a9
Roopesh R <44260545+LUKARS3@users.noreply.github.com>
2019-07-04 11:50:43 +0530

    Update prog-3.py

--- prog-3.py
+++ prog-3.py
@@ -1,6 +1,6 @@
-tup1 = ('u', 'a', 'o', 'i', 'e')
-ip1 = intput().lower()
-if ip1 in tup1:
+tup12 = ('u', 'a', 'o', 'i', 'e')
+ip12 = intput().lower()
+if ip12 in tup12:
   print('OVAL')
 else:
   print('Consonant')
