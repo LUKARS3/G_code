0fe8841773c815526e2ddefdcc1530bb1ddc027a
Roopesh R <44260545+LUKARS3@users.noreply.github.com>
2019-07-05 18:34:29 +0530

    Update prog-61.py

--- Pro/set7/prog-61.py
+++ Pro/set7/prog-61.py
@@ -3,15 +3,15 @@ r2 = input()
 de_string = ''
 lst = []
 for i in range(26):
-    lst.append(ord(i+97))
+    lst.append(chr(i+97))
 
-for i in range(len(s2)):
-    s2_pos = lst.index(r2[i]) + 1
-    s1_pos = lst.index(r1[i]) + 1
-    if s1_pos + s2_pos > 26:
-        pos = s1_pos - (26 - s2_pos)
+for i in range(len(r2)):
+    r2_pos = lst.index(r2[i]) + 1
+    r1_pos = lst.index(r1[i]) + 1
+    if r1_pos + r2_pos > 26:
+        pos = r1_pos - (26 - r2_pos)
         de_string += lst[pos-1]
     else:
-        pos = s1_pos + s2_pos
+        pos = r1_pos + r2_pos
         de_string += lst[pos-1]
 print(de_string)
