28a759eaaf64c5881cac8a3b6938f56cceaab37b
Luke <roopeshr084@gmail.com>
2019-07-09 18:25:31 +0530

    create set3

--- Pro/set1/prog-3.py
+++ Pro/set1/prog-3.py
@@ -1,7 +1,13 @@
 s1, s2 = map(str, input().split())
-zero_pop = len(s2)
+# zero_pop = len(s2)
+if len(s1) < len(s2):
+    s = len(s1)
+    zero_pop = len(s2)
+else:
+    s = len(s2)
+    zero_pop = len(s1)
 lst = list('0') * zero_pop
-for i in range(len(s1)):
+for i in range(s):
     if s1[i] == s2[i]:
         lst[i] = '1'
 
