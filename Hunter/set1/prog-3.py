80df3f87721cc04e377825f9fd4e380a93b452c0
Luke <roopeshr084@gmail.com>
2019-07-09 19:54:48 +0530

    create set3

---
+++ Hunter/set1/prog-3.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = [x for x in input().split()]
+lst1 = []
+for i in range(len(lst)):
+    if lst[i] == str(i):
+        lst1.append(lst[i])
+    # print(i, lst[i])
+
+if len(lst1) == 0:
+    print('-1')
+else:
+    print(' '.join(sorted(lst1)))
