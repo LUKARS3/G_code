8ea4b2fc18540b66a4787910740c13b07a221313
Luke <roopeshr084@gmail.com>
2019-07-09 19:31:35 +0530

    create set3

---
+++ Hunter/set1/prog-1.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = [x for x in input().split()]
+# lst = sorted(lst)
+lst1 = []
+for i in lst:
+    if lst.count(i) > 1:
+        lst1.append(i)
+
+if len(lst1) == 0:
+    print('unique')
+else:
+    print(' '.join(sorted(set(lst1))))
