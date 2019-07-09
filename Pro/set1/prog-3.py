0b3097bbbb11c331505629e726408aba2bb9fa6d
Luke <roopeshr084@gmail.com>
2019-07-09 18:45:47 +0530

    create set3

--- Pro/set1/prog-3.py
+++ Pro/set1/prog-3.py
@@ -8,6 +8,8 @@ else:
     zero_pop = len(s11)
 lst1 = list('0') * zero_pop
 for i1 in range(s):
+    if s22 in s11 or s11 in s22:
+        lst1[i1] = '1'
     if s11[i1] == s22[i1]:
         lst1[i1] = '1'
 
