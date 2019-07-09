f9a663880fe48263b97c2d0b2d1f0f74d5b5cee1
Luke <roopeshr084@gmail.com>
2019-07-09 18:16:16 +0530

    create set3

--- Pro/set 2/prog-15.py
+++ Pro/set 2/prog-15.py
@@ -1,8 +1,16 @@
+def so(l):
+    return l.count('1')
+
+
 n1 = input()
 lst1 = [int(x1) for x1 in input().split()]
 lst12 = []
+lst1 = sorted(lst1)
 for i in lst1:
     lst12.append(bin(i))
-lst12 = sorted(lst12, reverse=True)
-for i in lst12:
-    print(int(i, 2))
+# lst12 = sorted(lst12)
+lst12 = sorted(lst12, key=so)
+lst3 = lst12[::-1]
+for j in lst3:
+    print(int(j, 2))
+
